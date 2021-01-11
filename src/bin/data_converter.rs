30fb28e7b2148a6f5cd18445cfe5c4b99ff23e64
Zenthae <zenthae@gmail.com>
2021-01-11 22:08:30 +0100

    add data format converter

--- src/bin/data_converter.rs
+++ src/bin/data_converter.rs
@@ -1,15 +1,9 @@
-use std::{
-    env,
-    fs::{self, OpenOptions},
-    path::PathBuf,
-};
+use std::fs::{File, OpenOptions};
 
-use chrono::{DateTime, Utc};
-use key_logger_v2::Input;
+use chrono::{DateTime, Local, Utc};
+use key_logger_v2::{Input, RecordHolder};
 use serde_json::de::Deserializer;
 
-// const APP_NAME: &str = "Key Logger";
-// const APP_VERSION: &str = "2";
 const APP_ID: &str = "key_logger_v2";
 const VENDOR: &str = "Zenthae";
 const LOG_FILE_NAME: &str = "records.log";
@@ -23,16 +17,22 @@ fn main() {
     path.push(LOG_FILE_NAME);
 
     if let Ok(log_file) = OpenOptions::new().read(true).open(path) {
-        println!("File opened !");
+        // println!("File opened !");
+        let mut record_holder = RecordHolder::new();
         for (input, time) in Deserializer::from_reader(log_file)
             .into_iter::<(Input, DateTime<Utc>)>()
             .filter_map(|r| {
-                println!("{:?}", r);
+                // println!("{:?}", r);
                 r.ok()
             })
         {
-            println!("reading line");
-            println!("{:?} - {:?}", input, time);
+            // println!("{:?} - {:?}", input, time);
+            record_holder.entry(input).or_insert(Vec::new()).push(time);
+        }
+        if let Ok(data_file) =
+            File::create(format!("data-{}.json", Local::now().format("%F_%H.%M.%S")))
+        {
+            serde_json::to_writer_pretty(&data_file, &record_holder).expect("u mad bro ?");
         }
     }
 }
